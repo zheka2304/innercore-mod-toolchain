e48b43dc81a87f9c85a1cce047bfa880046bb820
MaXFeeD <maksfeed.mf@gmail.com>
2022-09-25 23:07:37 +0300

    Rewrited importing script, now it supports most toolchain features

--- toolchain/toolchain/python/import.py
+++ toolchain/toolchain/python/import.py
@@ -1,201 +1,213 @@
 import os
-from os.path import exists, join, basename, isfile, isdir
+from os.path import exists, join, basename, isfile, isdir, relpath
 import sys
 import json
-import platform
 
-from utils import clear_directory, copy_directory, ensure_directory, copy_file
+from utils import copy_directory, copy_file, ensure_directory
 from base_config import BaseConfig
-from setup_commons import cleanup_if_required
-from project_manager import PROJECT_MANAGER
-from make_config import MAKE_CONFIG
 
-root_files = []
-
-def import_mod_info(make_file, directory):
-	global root_files
-	root_files.append("mod.info")
-
+def load_mod_info(make_obj, directory):
 	mod_info = join(directory, "mod.info")
-
-	if not exists(mod_info):
-		from project_manager_tasks import create_project
-		folder = create_project(True)
-		PROJECT_MANAGER.select_project(folder=folder)
-		return folder
-
-	with open(mod_info, "r", encoding="utf-8") as info_file:
-		info_obj = json.loads(info_file.read())
-		index = PROJECT_MANAGER.create_project(
-	  		info_obj.get("name", "Unknown Mod"),
-			author = info_obj.get("author", ""),
-			version = info_obj.get("version", "1.0"),
-			description = info_obj.get("description", "")
-   		)
-		PROJECT_MANAGER.select_project(index=index)
-		return PROJECT_MANAGER.get_folder(index=index)[1]
-
-def import_build_config(make_file, folder, source, destination):
-	global root_files
-	root_files.append("build.config")
-
-	build_config = join(source, "build.config")
-	with open(build_config, "r", encoding="utf-8") as config_file:
-		config_obj = json.loads(config_file.read())
-		config = BaseConfig(config_obj)
-		make_file["info"]["api"] = config.get_value("defaultConfig.api", "CoreEngine")
-
-		src_dir = join(destination, folder)
-
-		# clear assets folder
-		assets_dir = join(src_dir, "assets")
-		clear_directory(assets_dir)
-		os.makedirs(assets_dir)
-
-		# some pre-defined resource folders
-		resources = [
-			{
-				"path": "assets/resource_packs/*",
-				"type": "minecraft_resource_pack"
-			},
-			{
-				"path": "assets/behavior_packs/*",
-				"type": "minecraft_behavior_pack"
-			}
-		]
-
-		os.makedirs(join(assets_dir, "resource_packs"))
-		os.makedirs(join(assets_dir, "behavior_packs"))
-
-		# import assets
-		for res_dir in config.get_filtered_list("resources", "resourceType", ("resource", "gui")):
-			if res_dir["resourceType"] == "resource":
-				res_dir["resourceType"] = "resource_directory"
-			path_stripped = res_dir["path"].strip("/")
-			path_parts = path_stripped.split("/")
-			path = join(*path_parts)
-			copy_directory(join(source, path), join(assets_dir, path), True)
-			resources.append({
-				"path": "assets/" + path_stripped,
-				"type": res_dir["resourceType"]
-			})
-			root_files.append(path_parts[0])
-
-		make_file["resources"] = resources
-
-		# clear libraries folder and copy libraries from the old project
-		libs_dir = join(destination, folder, "lib")
-		clear_directory(libs_dir)
-		clear_directory(join(destination, folder, "dev"))
-		os.makedirs(libs_dir)
-		old_libs = config.get_value("defaultConfig.libraryDir", "lib").strip("/")
-		old_libs_parts = old_libs.split("/")
-		old_libs_dir = join(source, *old_libs_parts)
-		if isdir(old_libs_dir):
-			root_files.append(old_libs_parts[0])
-			copy_directory(old_libs_dir, libs_dir)
-
-		# some pre-defined source folders
-		sources = [
-			{
-				"source": "lib/*",
-				"type": "library",
-				"language": "javascript"
-			},
-			{
-				"source": "preloader/*",
-				"type": "preloader",
-				"language": "javascript"
-			}
-		]
-
-		ensure_directory(join(src_dir, "preloader"))
-
-		# import sources
-		for source_dir in config.get_filtered_list("compile", "sourceType", ("mod", "launcher", "preloader", "instant", "custom", "library")):
-			if source_dir["sourceType"] == "mod":
-				source_dir["sourceType"] = "main"
-
-			toolchain_source = {
-				"type": source_dir["sourceType"],
-				"language": "javascript"
-			}
-
-			if "api" in source_dir:
-				toolchain_source["api"] = source_dir["api"]
-			if "optimizationLevel" in source_dir:
-				toolchain_source["optimizationLevel"] = source_dir["optimizationLevel"]
-			if "sourceName" in source_dir:
-				toolchain_source["sourceName"] = source_dir["sourceName"]
-
-			source_parts = source_dir["path"].split("/")
-			root_files.append(source_parts[0])
-
-			build_dirs = config.get_filtered_list("buildDirs", "targetSource", (source_dir["path"]))
-			if len(build_dirs) > 0:
-				old_build_path = build_dirs[0]["dir"].strip("/")
-				old_path_parts = old_build_path.split("/")
-				toolchain_source["source"] = old_build_path
-				toolchain_source["target"] = source_dir["path"]
-				root_files.append(old_path_parts[0])
-				copy_directory(join(source, *old_path_parts), join(src_dir, *old_path_parts), True)
-			else:
-				toolchain_source["source"] = source_dir["path"]
-				copy_file(join(source, *source_parts), join(src_dir, *source_parts))
-
-			sources.append(toolchain_source)
-
-		make_file["sources"] = sources
+	mod_info_obj = {
+		"name": basename(directory if directory != "." else os.getcwd())
+	} if not "info" in make_obj else make_obj["info"]
+	if not exists(mod_info) or not isfile(mod_info):
+		make_obj["info"] = mod_info_obj
 		return
-	print("Unable to read build.config!", file=sys.stderr)
 
-def copy_additionals(source, folder, destination):
-	global root_files
+	with open(mod_info, "r", encoding="utf-8") as mod_info_file:
+		mod_info_prototype = json.loads(mod_info_file.read())
+		if "name" in mod_info_prototype:
+			mod_info_obj["name"] = mod_info_prototype["name"]
+		if "version" in mod_info_prototype:
+			mod_info_obj["version"] = mod_info_prototype["version"]
+		if "author" in mod_info_prototype:
+			mod_info_obj["author"] = mod_info_prototype["author"]
+		if "description" in mod_info_prototype:
+			mod_info_obj["description"] = mod_info_prototype["description"]
+	make_obj["info"] = mod_info_obj
 
-	files = os.listdir(source)
-	for filename in files:
-		if filename in root_files:
+def load_build_config(make_obj, source, destination):
+	build_config = join(source, "build.config")
+	destination_copy_tuples = []
+	if not exists(build_config) or not isfile(build_config):
+		return destination_copy_tuples
+
+	with open(build_config, "r", encoding="utf-8") as build_config_file:
+		build_config_obj = json.loads(build_config_file.read())
+		config = BaseConfig(build_config_obj)
+
+		default_config_api = config.get_value("defaultConfig.api")
+		if default_config_api is not None or not "api" in make_obj:
+			make_obj["api"] = default_config_api if default_config_api is not None else "CoreEngine"
+		default_config_optimization_level = config.get_value("defaultConfig.optimizationLevel")
+		if default_config_optimization_level is not None:
+			make_obj["optimizationLevel"] = default_config_optimization_level
+		default_config_setup_script = config.get_value("defaultConfig.setupScript")
+		if default_config_setup_script is not None:
+			make_obj["setupScript"] = default_config_setup_script
+			destination_copy_tuples.append((join(source, default_config_setup_script), join(destination, default_config_setup_script)))
+
+		assets_dir = join(destination, "assets")
+		if not "resources" in make_obj:
+			make_obj["resources"] = []
+
+		for directory in config.get_filtered_list("resources", "resourceType", ("resource", "gui")):
+			if directory["resourceType"] == "resource":
+				directory["resourceType"] = "resource_directory"
+			path_stripped = directory["path"].strip("/")
+			path = join(*path_stripped.split("/"))
+			destination_copy_tuples.append((join(source, path), join(assets_dir, path)))
+			make_obj["resources"].append({
+				"path": "assets/" + path_stripped,
+				"type": directory["resourceType"]
+			})
+
+		libs = config.get_value("defaultConfig.libraryDir", "lib").strip("/")
+		libs_directory = join(source, *libs.split("/"))
+		if isdir(libs_directory):
+			destination_copy_tuples.append((libs_directory, join(destination, "lib")))
+			if not "sources" in make_obj or len(make_obj["sources"]) == 0:
+				make_obj["sources"] = [
+					{
+						"source": "lib/*",
+						"type": "library",
+						"language": "javascript"
+					}
+				]
+		elif not "sources" in make_obj:
+			make_obj["sources"] = []
+
+		for directory in config.get_filtered_list("compile", "sourceType", ("mod", "launcher", "preloader", "instant", "custom", "library")):
+			if directory["sourceType"] == "mod":
+				directory["sourceType"] = "main"
+			toolchain_source = {
+				"type": directory["sourceType"],
+				"language": "javascript"
+			}
+			if "api" in directory:
+				toolchain_source["api"] = directory["api"]
+			if "optimizationLevel" in directory:
+				toolchain_source["optimizationLevel"] = directory["optimizationLevel"]
+			if "sourceName" in directory:
+				toolchain_source["sourceName"] = directory["sourceName"]
+			path_stripped = directory["path"].split("/")
+			build_dirs = config.get_filtered_list("buildDirs", "targetSource", (directory["path"]))
+			if len(build_dirs) > 0:
+				toolchain_source["source"] = build_dirs[0]["dir"].strip("/")
+				build_path_stripped = toolchain_source["source"].split("/")
+				toolchain_source["target"] = directory["path"]
+				destination_copy_tuples.append((join(source, *build_path_stripped), join(destination, *build_path_stripped)))
+			else:
+				toolchain_source["source"] = directory["path"]
+				destination_copy_tuples.append((join(source, *path_stripped), join(destination, *path_stripped)))
+			make_obj["sources"].append(toolchain_source)
+
+		if not "compile" in make_obj:
+			make_obj["compile"] = []
+
+		for directory in config.get_value("javaDirs", []):
+			path_stripped = directory["path"].strip("/")
+			path = join(source, *path_stripped.split("/"))
+			destination_copy_tuples.append((path, join(destination, "java", basename(path))))
+			make_obj["compile"].append({
+				"source": "java/" + basename(path),
+				"type": "java"
+			})
+
+		for directory in config.get_value("nativeDirs", []):
+			path_stripped = directory["path"].strip("/")
+			path = join(source, *path_stripped.split("/"))
+			destination_copy_tuples.append((path, join(destination, "native", basename(path))))
+			make_obj["compile"].append({
+				"source": "native/" + basename(path),
+				"type": "native"
+			})
+
+		if not "additional" in make_obj or len(make_obj["additional"]) == 0:
+			make_obj["additional"] = [
+				{
+					"source": "assets/root/*",
+					"targetDir": "."
+				}
+			]
+
+	return destination_copy_tuples
+
+def copy_tuple_directories(tuples, source, destination):
+	ignore_list = ["make.json", "mod_icon.png", "mod.info", "build.config", "assets/root"]
+	for path, output in tuples:
+		if path == output:
 			continue
-		src = join(source, filename)
-		dest = join(destination, folder, "assets", "root")
-		if isfile(src):
-			copy_file(src, join(dest, filename))
-		elif isdir(src):
-			copy_directory(src, join(dest, filename))
+		if isfile(path) and (not exists(output) or isfile(output)):
+			if exists(output):
+				print("Replacing", basename(output))
+			copy_file(path, output)
+		elif isdir(path) and (not exists(output) or isdir(output)):
+			if exists(output):
+				print("Merging", basename(output))
+			copy_directory(path, output)
+		else:
+			print("Destination and input", basename(input), "pathes conflicts!")
+		ignore_list.append(relpath(path, source))
+	mod_icon = join(source, "mod_icon.png")
+	if exists(mod_icon) and isfile(mod_icon):
+		copy_file(mod_icon, join(destination, "mod_icon.png"))
+	additional_path = join(destination, "assets", "root")
+	copy_directory(source, additional_path, ignore_list=ignore_list)
+	for dirpath, dirnames, filenames in os.walk(additional_path, False):
+		for dirname in dirnames:
+			directory = join(dirpath, dirname)
+			if len(os.listdir(directory)) == 0:
+				os.rmdir(directory)
+	if len(os.listdir(additional_path)) == 0:
+		os.removedirs(additional_path)
 
 
-destination = sys.argv[1]
-source = sys.argv[2]
-toolchain_path = MAKE_CONFIG.filename
+if __name__ == "__main__":
+	if len(sys.argv) <= 1 or sys.argv[1] == "--help":
+		print("Usage: import.py <destination> [path]")
+		print("Performs conversion between mod.info, build.config and make.json, merges directories if few configurations exists.")
+		exit(0)
 
-if not (exists(toolchain_path) and exists(source)):
-	from task import error
-	error("Usage: \r\n" + ("python" if platform.system() == "Windows" else "python3") + " import.py <destination> <source>")
+	destination = sys.argv[1]
+	path = sys.argv[2] if len(sys.argv) > 2 else sys.argv[1]
 
-with open(toolchain_path, "r", encoding="utf-8") as make_file:
-	make_obj = json.loads(make_file.read())
+	if exists(path) and isfile(path):
+		path = join(path, "..")
+	if not exists(path):
+		print("Input path not exists or specified!")
+		exit(1)
+	if exists(destination) and isfile(destination):
+		print("Destination path is file!")
+		exit(2)
+	make_obj = {}
 
-if source == ".":
-	dirname = basename(os.getcwd())
-else:
-	dirname = basename(source)
+	make_project = join(destination, "make.json")
+	if exists(make_project) and isfile(make_project):
+		print("Merging with output make.json")
+		with open(make_project, "r", encoding="utf-8") as make_file:
+			make_obj = json.loads(make_file.read())
 
-print("Importing mod.info")
-folder = import_mod_info(make_obj, source)
-with open(MAKE_CONFIG.get_path("make.json"), "r", encoding="utf-8") as make_file:
-	make_project_obj = json.loads(make_file.read())
+	make = join(path, "make.json")
+	if exists(make) and isfile(make):
+		print("Reading existing make.json")
+		with open(make, "r", encoding="utf-8") as make_file:
+			make_obj = json.loads(make_file.read())
 
-print("Importing build.config")
-import_build_config(make_project_obj, folder, source, destination)
+	print("Resolving mod.info")
+	load_mod_info(make_obj, path)
 
-with open(MAKE_CONFIG.get_path("make.json"), "w", encoding="utf-8") as make_file:
-	make_file.write(json.dumps(make_project_obj, indent="\t") + "\n")
+	print("Resolving build.config")
+	tuples = load_build_config(make_obj, path, destination)
 
-print("Copying additional files and directories")
-copy_additionals(source, folder, destination)
-cleanup_if_required(destination)
+	print("Flushing make.json")
+	ensure_directory(destination)
+	with open(make_project, "w", encoding="utf-8") as make_file:
+		make_file.write(json.dumps(make_obj, indent="\t") + "\n")
 
-with open(toolchain_path, "w", encoding="utf-8") as make_file:
-	make_file.write(json.dumps(make_obj, indent="\t") + "\n")
+	if destination != path or input("Do you want to copy reassigned directories in directory itself? [N/y]: ").lower() == "y":
+		print("Copying files and directories")
+		copy_tuple_directories(tuples, path, destination)
 
-print("Project successfully imported, please, delete project.back after triple checking that everything is OK")
+	print("Project successfully imported!")
