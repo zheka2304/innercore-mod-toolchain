6ba8a1da156efb3f80d5f4b5879add044c464042
MaXFeeD <maksfeed.mf@gmail.com>
2022-09-25 23:05:56 +0300

    Improved copy assignment

--- toolchain/toolchain/python/utils.py
+++ toolchain/toolchain/python/utils.py
@@ -1,7 +1,10 @@
 import os
 from os.path import join, exists, abspath, isfile, isdir
+import shutil
 
 def ensure_directory(directory):
+	if isfile(directory):
+		os.remove(directory)
 	if not exists(directory):
 		os.makedirs(directory)
 
@@ -16,7 +19,10 @@ def clear_directory(directory):
 def copy_file(src, dst):
 	import shutil
 	ensure_file_dir(dst)
-	shutil.copy(src, dst)
+	try:
+		shutil.copy(src, dst)
+	except shutil.SameFileError:
+		pass
 
 def move_file(src, dst):
 	import shutil
@@ -29,30 +35,31 @@ def index_of(_list, _value):
 	except ValueError:
 		return -1
 
-def copy_directory(src, dst, clear_dst = False, replacement = True, ignore = [], ignore_list = []):
-	ensure_directory(dst)
+def copy_directory(path, destination, clear_dst = False, replacement = True, ignore_list = [], relative_path = None):
+	ensure_directory(destination)
 	if clear_dst:
-		clear_directory(dst)
-
-	if not exists(dst):
-		os.makedirs(dst)
-	from glob import glob
-
-	if len(ignore) > 0:
-		for i in ignore:
-			ignore_list += glob(join(dst, i))
-
-	import shutil
-	for item in os.listdir(src):
-		s = join(src, item)
-		d = join(dst, item)
-		if isfile(s) and exists(d) and not replacement:
+		clear_directory(destination)
+	if not exists(destination):
+		os.makedirs(destination)
+	for filename in os.listdir(path):
+		relative_file = join(relative_path, filename) if relative_path is not None else filename
+		if filename in ignore_list or relative_file in ignore_list:
 			continue
-
-		if isdir(s):
-			copy_directory(s, d, clear_dst, replacement, ignore_list=ignore_list)
-		elif index_of(ignore_list, d) == -1:
-			shutil.copy2(s, d)
+		input = join(path, filename)
+		if input in ignore_list:
+			continue
+		output = join(destination, filename)
+		if isfile(input) and exists(output) and not replacement:
+			continue
+		if isdir(input):
+			if isfile(output):
+				os.remove(output)
+			copy_directory(input, output, clear_dst, replacement, ignore_list, relative_file)
+		elif not isdir(output):
+			try:
+				shutil.copy2(input, output)
+			except shutil.SameFileError:
+				pass
 
 def get_all_files(directory, extensions = ()):
 	all_files = []
