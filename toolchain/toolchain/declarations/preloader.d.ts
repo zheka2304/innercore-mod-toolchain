29b8e284f412624211159a2c1d0b41058339ee35
MaXFeeD <maksfeed.mf@gmail.com>
2022-09-26 22:30:38 +0300

    AdaptedScript API

--- toolchain/toolchain/declarations/preloader.d.ts
+++ toolchain/toolchain/declarations/preloader.d.ts
@@ -1,4 +1,4 @@
-/// <reference path="./innercore.d.ts"/>
+/// <reference path="./adapted-script.d.ts"/>
 
 /**
  * @returns PreloaderAPI level, default is 1
@@ -20,7 +20,6 @@ declare function log(message: string): void
 */
 declare function print(str: string): void
 
-
 declare namespace Resources {
 
 	function addRuntimePack(typeStr: string, name: string): string
@@ -40,27 +39,5 @@ declare namespace Resources {
 	function getResourcePathNoVariants(path: string): java.io.File | null
 
 	function getResourcePath(path: string): string | null
-
-}
-
-declare namespace Callback {
-
-	/**
-	 * Adds callback function for the specified callback name. Most of native
-	 * events can be prevented using [[Game.prevent]] call.
-	 * @param name callback name, should be one of the pre-defined or a custom
-	 * name if invoked via [[Callback.invokeCallback]]
-	 * @param func function to be called when an event occures
-	 */
-	function addCallback(name: string, func: Function, priority: number): void
-
-	/**
-	 * Invokes callback with any name and up to 10 additional parameters. You
-	 * should not generally call pre-defined callbacks until you really need to
-	 * do so. If you want to trigger some event in your mod, use your own
-	 * callback names
-	 * @param name callback name
-	 */
-	function invokeCallback(name: string, o1?: any, o2?: any, o3?: any, o4?: any, o5?: any, o6?: any, o7?: any, o8?: any, o9?: any, o10?: any): void
 
 }
